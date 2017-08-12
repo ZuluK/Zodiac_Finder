c8962e97de74ff3402d9a8bf0aff652fae609020
Richard Spence <richardspence@Richards-MacBook-Air.local>
2017-08-11 18:04:23 -0700

    Display changes

--- js/main.js
+++ js/main.js
@@ -3,63 +3,63 @@
  var zodiac  = [
    {
      sign: "aries",
-     text: "The Good: Energetic, encouraging, unstoppable, bold, devoted, heroic, caring. The Bad: Proud, self-centered, impulsive, bossy, stubborn, reckless, jealous",
+     text: "Energetic, encouraging, unstoppable, bold, devoted, heroic, caring.",
      image:"img/aries.jpg"
 
    },
    {
      sign: "aquarius",
-     text: "The Good: Communicative, original, open-minded, fair, logical, inviting. The Bad: Guarded, detached, self-destructive, out-of-touch, irrational, desperate",
+     text: "Communicative, original, open-minded, fair, logical, inviting.",
      image:"img/aquarius.jpg"
    },
    {
      sign: "libra",
-     text: "The Good: Charming, lovable, fair, sincere, sharing, hopelessly romantic. The Bad: Vain, indecisive, melodramatic, manipulative, spoiled, delusional",
+     text: "Charming, lovable, fair, sincere, sharing, hopelessly romantic.",
      image: "img/libra.jpg"
    },
    {
      sign: "taurus",
-     text: "The Good: Patient, organized, supportive, romantic, careful, dedicated. The Bad: Overindulgent, stubborn, lazy, vain, cheap, too cautious",
+     text: "Patient, organized, supportive, romantic, careful, dedicated.",
      image: "img/taurus.jpg"
    },
    {
      sign: "scorpio",
-     text: "The Good: Magnetic, passionate, loyal, protective, trendsetting, brave. The Bad: Obsessive, possessive, jealous, secretive, vengeful, manipulative",
+     text: "The Good: Magnetic, passionate, loyal, protective, trendsetting, brave.",
      image: "img/scorpio.jpg"
    },
    {
      sign: "gemini",
-     text: "The Good: Fascinating, original, resourceful, charming, wise, adventurous. The Bad: Restless, distracted, two-faced, judgmental, depressed, overwhelmed",
+     text: "Fascinating, original, resourceful, charming, wise, adventurous.",
      image: "img/gemini.jpg"
    },
    {
      sign: "sagittarius",
-     text: "The Good:  Honest, fair-minded, inspiring, optimistic, enthusiastic, encouraging, dedicated. The Bad: Argumentative, reckless, flaky, preachy, tactless, overconfident",
+     text: "Honest, fair-minded, inspiring, optimistic, enthusiastic, encouraging, dedicated.",
      image: "img/sagittaruis.jpg"
    },
    {
      sign: "cancer",
-     text: "The Good: Helpful, patient, compassionate, nurturing, romantic, creative. The Bad: Gossipy, clique-y, isolated, uncommunicative, hypersensitive, overly competitive",
+     text: "Helpful, patient, compassionate, nurturing, romantic, creative.",
      image: "img/cancer.jpg"
    },
    {
      sign: "capricorn",
-     text: "The Good: Loyal, family-minded, hardworking, devoted, honest, fearless. The Bad: Pessimistic, unforgiving, cold, materialistic, snobbish, hopeless",
+     text: "Loyal, family-minded, hardworking, devoted, honest, fearless.",
      image: "img/capircorn.jpg"
    },
    {
      sign: "leo",
-     text: "The Good: Courageous, kind, generous, loyal, protective, nakedly honest, entertaining. The Bad: Arrogant, wasteful, sloppy, cold-hearted, jealous, aggressive",
+     text: "Courageous, kind, generous, loyal, protective, nakedly honest, entertaining.",
      image: "img/leo.jpg"
    },
    {
      sign: "virgo",
-     text: "The Good:  Dedicated, resourceful, helpful, hardworking, witty, practical. The Bad: Preachy, self-destructive, overwhelmed, self-pitying, uptight, critical",
+     text: "Dedicated, resourceful, helpful, hardworking, witty, practical.",
      image: "img/virgo.jpg"
    },
    {
      sign: "pisces",
-     text: "The Good:  Romantic, helpful, wise, comforting, imaginative. The Bad: Gullible, self-pitying, out of touch with reality, self-destructive, clingy",
+     text: "Romantic, helpful, wise, comforting, imaginative.",
      image: "img/pisces.jpg"
    }
  ];
